3784e6a9db24ffc0b3a8441aa2d60bedd038584b
shuvoooo <shuvo.punam@gmail.com>
2022-02-06 16:58:17 +0600

    Link added in Homepage

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\StripePaymentController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('stripe', [StripePaymentController::class, 'stripe'])->name('stripe');
 Route::post('stripe', [StripePaymentController::class, 'stripePost'])->name('stripe.pay');
 
